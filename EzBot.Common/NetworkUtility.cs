259bb7963b9f1bbfbbd96efaefde53962e7aeb2f
ceezar <zeljkolamesic1170@gmail.com>
2025-01-14 12:20:34 +0100

    added polly for http requests

--- EzBot.Common/NetworkUtility.cs
+++ EzBot.Common/NetworkUtility.cs
@@ -1,32 +1,23 @@
-using System.Net.Sockets;
+using Polly;
+using Polly.Retry;
 
 namespace EzBot.Common;
-
 public static class NetworkUtility
 {
-    private static async Task<bool> IsInternetAvailableAsync()
-    {
-        try
-        {
-            using var client = new TcpClient();
-            await client.ConnectAsync("8.8.8.8", 53); // Google Public DNS
-            return true;
-        }
-        catch
-        {
-            return false;
-        }
-    }
+    private static readonly AsyncRetryPolicy<HttpResponseMessage> RetryPolicy = Policy
+        .Handle<HttpRequestException>()
+        .OrResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+        .WaitAndRetryAsync(
+            retryCount: 3,
+            sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+            onRetry: (outcome, timespan, retryAttempt, context) =>
+            {
+                // Log the retry attempt
+                Console.WriteLine($"Retrying... Attempt {retryAttempt}");
+            });
 
     public static async Task<HttpResponseMessage> MakeRequestAsync(HttpClient httpClient, HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        while (!await IsInternetAvailableAsync())
-        {
-            // Console.WriteLine("Internet is down. Retrying in 5 seconds...");
-
-            await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
-        }
-
-        return await httpClient.SendAsync(request, cancellationToken);
+        return await RetryPolicy.ExecuteAsync(() => httpClient.SendAsync(request, cancellationToken));
     }
 }
